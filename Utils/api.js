87041f5294fbf2c1a8d351f490769dfbb1487893
raj-subhankar <subhankar.rj@gmail.com>
2016-08-26 12:44:43 +0530

    Fix url

--- Utils/api.js
+++ Utils/api.js
@@ -1,12 +1,12 @@
 var api = {
   getBio(username) {
     username = username.toLowerCase().trim();
-    var url = `https://github.com/users/${username}`;
+    var url = `https://api.github.com/users/${username}`;
     return fetch(url).then((res) => res.json());
   },
   getRepos(username) {
     username = username.toLowerCase().trim();
-    var url = `https://github.com/users/${username}/repos`;
+    var url = `https://api.github.com/users/${username}/repos`;
     return fetch(url).then((res) => res.json());
   }
 };
