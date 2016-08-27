ee4265f566ae06b7978dd15c63a115ece37c638c
raj-subhankar <subhankar.rj@gmail.com>
2016-08-27 17:06:30 +0530

    Create component Profile

---
+++ App/Components/Profile.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+var Badge = require('./Badge');
+
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView
+} from 'react-native';
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  rowContainer: {
+    padding: 10
+  },
+  rowTitle: {
+    color: '#48BBEC',
+    fontSize: 16
+  },
+  rowContent: {
+    fontSize: 19
+  }
+});
+
+class Profile extends Component {
+  getRowTitle(user, item) {
+    item = (item === 'public_repos') ? item.replace('_', ' ') : item;
+    return item[0] ? item[0].toUpperCase() + item.slice(1) : item;
+  }
+  render() {
+    var userInfo = this.props.userInfo;
+    var topicArr = ['company', 'location', 'followers', 'following', 'email', 'bio', 'public_repos'];
+    var list = topicArr.map((item, index) =>{
+      if(!userInfo[item]){
+        return <View key = {index} />
+      } else {
+        return (
+          <View key = {index}>
+            <View style = {styles.rowContainer}>
+              <Text style = {styles.rowTitle}> {this.getRowTitle(userInfo, item)} </Text>
+              <Text style = {styles.rowContent}> {userInfo[item]} </Text>
+            </View>
+          </View>
+        )
+      }
+    });
+    return (
+      <ScrollView style = {styles.container}>
+        <Badge userInfo = {this.props.userInfo} />
+        {list}
+      </ScrollView>
+    )
+  }
+};
+
+module.exports = Profile;
