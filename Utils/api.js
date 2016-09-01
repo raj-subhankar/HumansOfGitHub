145861194aed730f12b09cf1d90d2d212e6bf1d7
raj-subhankar <subhankar.rj@gmail.com>
2016-09-01 23:33:24 +0530

    Create getNotes() and addNote()

--- Utils/api.js
+++ Utils/api.js
@@ -8,6 +8,19 @@ var api = {
     username = username.toLowerCase().trim();
     var url = `https://api.github.com/users/${username}/repos`;
     return fetch(url).then((res) => res.json());
+  },
+  getNotes(username) {
+    username = username.toLowerCase().trim();
+    var url = 'https://humansofgithub.firebaseio.com/${username}.json';
+    return fetch(url).then((res) => res.json());
+  },
+  addNote(username, note) {
+    username = username.toLowerCase().trim();
+    var url = 'https://humansofgithub.firebaseio.com/${username}.json';
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then((res) => res.json())
   }
 };
 
